28c790ae62d18c1827952500428600bd4a4a9016
map17 <deciduouscanopy@gmail.com>
2017-09-04 19:02:27 +0100

    Update

--- modal.js
+++ modal.js
@@ -3,8 +3,10 @@ function pop( fileIndex ) {
     document.getElementById( 'TheModalContent' ).innerHTML = '<object width=100% height=100% type="text/html" data="' + fileIndex + '" ></object>';
     
     document.getElementById( 'TheModal' ).style.display = 'block';
+    $("body").css("overflow", "hidden");
 }
 
 function hide() {
     document.getElementById( 'TheModal' ).style.display = 'none';
+    $("body").css("overflow", "scroll");
 }
