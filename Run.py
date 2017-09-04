b1497d28d59516b3ed9584894ae80e8345cd23e7
map17 <deciduouscanopy@gmail.com>
2017-09-04 18:01:25 +0100

    Update

--- Run.py
+++ Run.py
@@ -186,7 +186,7 @@ for row in csvReader:
         foutIndividual.write( '    <div class="mySlides fade">\n' )
         foutIndividual.write( '      <div class="numbertext">{:d} of {:d}</div>\n'.format( 1 + iImage,
                                                                                            nImages ) )
-        foutIndividual.write( '      <img src="../../{:s}" style="height:128px;">\n'.format( fileDestImage ) )
+        foutIndividual.write( '      <img src="../../{:s}" style="width:100%;">\n'.format( fileDestImage ) )
         #foutIndividual.write( '      <div class="text">Figure {:d}</div>\n'.format( 1 + iImage ) )
         foutIndividual.write( '    </div>\n\n' )
 
