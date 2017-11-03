51b864e1f8bbb7f45f406a9465e9a927c5255f29
map17 <deciduouscanopy@gmail.com>
2017-11-03 11:43:27 +0000

    Update

--- individuals/Villette/Thumb_Mod.sh
+++ individuals/Villette/Thumb_Mod.sh
@@ -1,1 +1,1 @@
-convert "/Users/john/Documents/UAL-LCC/MAP17.Website.Content/Villette/*4658*" -flatten -resize 1024x1024^ -gravity center -crop 1024x64+0+0 +repage "individuals/Villette/Thumb.jpg"
+convert "/Users/john/Documents/UAL-LCC/MAP17.Website.Content/Villette/*Uranium*" -flatten -resize 1024x1024^ -gravity center -crop 1024x64+0+0 +repage "individuals/Villette/Thumb.jpg"
