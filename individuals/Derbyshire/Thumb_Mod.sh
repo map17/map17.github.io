f9708db94137c9ebbc48117f4c107a3add885f7b
map17 <deciduouscanopy@gmail.com>
2017-11-21 19:28:27 +0000

    Update

--- individuals/Derbyshire/Thumb_Mod.sh
+++ individuals/Derbyshire/Thumb_Mod.sh
@@ -1,1 +1,1 @@
-convert "/Users/john/Documents/UAL-LCC/MAP17.Website.Content/Derbyshire/Julie Derbyshire, Pietra Paesina, 2017 (1).tif" -flatten -resize 1024x1024^ -gravity center -crop 1024x64+0+250 +repage "individuals/Derbyshire/Thumb.jpg" ; open individuals/Derbyshire/Thumb.jpg
+convert "/Users/john/Documents/UAL-LCC/MAP17.Website.Content.Old/Derbyshire/OptionalSelectedImages/Julie Derbyshire, Pietra Paesina, 2017 (1).tif" -flatten -resize 1024x1024^ -gravity center -crop 1024x64+0+250 +repage "individuals/Derbyshire/Thumb.jpg" ; open individuals/Derbyshire/Thumb.jpg
