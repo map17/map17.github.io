5d5c88bdb0716fe033ba8c4327385e9c5e8b4f5d
map17 <deciduouscanopy@gmail.com>
2017-11-21 20:19:03 +0000

    Update

--- individuals/Hipwell/Thumb_Mod.sh
+++ individuals/Hipwell/Thumb_Mod.sh
@@ -1,1 +1,1 @@
-convert "/Users/john/Documents/UAL-LCC/MAP17.Website.Content/Hipwell/John Hipwell Standing Wave, Change Hands 2017 05.tiff" -flatten -resize 1024x1024^ -gravity center -crop 1024x64+0+0 +repage "individuals/Hipwell/Thumb.jpg"
+convert "/Users/john/Documents/UAL-LCC/MAP17.Website.Content/Hipwell/John Hipwell Standing Wave, Change Hands 2017 07.tiff" -flatten -resize 1024x1024^ -gravity center -crop 1024x64+0+0 +repage "individuals/Hipwell/Thumb.jpg"
